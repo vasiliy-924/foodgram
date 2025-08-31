5d0a55531bfc333ee9bc4a271bee84c373af606c
Vasiliy Petrov <vasiliy@vppetrov.ru>
2025-08-31 13:22:03 +0300

    ver 1.0.3

--- frontend/src/setupTests.js
+++ frontend/src/setupTests.js
@@ -3,3 +3,11 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+
+// Polyfills for JSDOM under Node 21: ensure timers exist for testing-library/jest
+if (typeof global.setImmediate === 'undefined') {
+  global.setImmediate = (callback, ...args) => setTimeout(callback, 0, ...args);
+}
+if (typeof global.clearImmediate === 'undefined') {
+  global.clearImmediate = (handle) => clearTimeout(handle);
+}
