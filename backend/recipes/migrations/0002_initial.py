6b3ab185afecea0a5843636a2ea5faab873cfc2a
Vasiliy Petrov <vasiliy@vppetrov.ru>
2025-09-02 22:44:00 +0300

    w1.3.3

--- backend/recipes/migrations/0002_initial.py
+++ backend/recipes/migrations/0002_initial.py
@@ -7,6 +7,8 @@ from django.db import migrations, models
 
 class Migration(migrations.Migration):
 
+    initial = True
+
     dependencies = [
         ('recipes', '0001_initial'),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
